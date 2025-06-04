a55fe9f0a826c18a28e47cf6c038182ca02b9a02
Lucas Marques <lucasmarquesff@hotmail.com>
2025-06-04 14:18:02 -0300

    First commit

---
+++ src/main/java/br/com/fiap/FarmaNear_Register/FarmaNearRegisterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FarmaNear_Register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaNearRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaNearRegisterApplication.class, args);
+	}
+
+}
