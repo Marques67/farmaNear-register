63007f482fee545740d6dcebd0605424d4b8b633
Lucas Marques <lucasmarquesff@hotmail.com>
2025-06-27 16:55:13 -0300

    Fixing csv method

--- src/main/java/br/com/fiap/FarmaNear_Register/usecases/UploadCsvUseCase.java
+++ src/main/java/br/com/fiap/FarmaNear_Register/usecases/UploadCsvUseCase.java
@@ -6,9 +6,9 @@ import br.com.fiap.FarmaNear_Register.interfaces.IProductJpaGateway;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,12 +21,12 @@ public class UploadCsvUseCase {
     }
 
     public List<ProductDto> uploadCsv(MultipartFile file) throws IOException {
-        if (file.isEmpty() || !file.getOriginalFilename().endsWith(".csv")) {
-            throw new IllegalArgumentException("Invalid file. Submit a CSV.");
-        }
+        List<String> lines = transformFileInString(file);
 
-        List<ProductCsvDto> csvProducts = new CsvToBeanBuilder<ProductCsvDto>(
-                new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))
+        String csvContent = String.join("\n", lines);
+        Reader cleanReader = new StringReader(csvContent);
+
+        List<ProductCsvDto> csvProducts = new CsvToBeanBuilder<ProductCsvDto>(cleanReader)
                 .withType(ProductCsvDto.class)
                 .withSeparator(';')
                 .withQuoteChar('"')
@@ -50,5 +50,26 @@ public class UploadCsvUseCase {
         productJpaGateway.saveProducts(productList);
 
         return productList;
+    }
+
+    private static List<String> transformFileInString(MultipartFile file) throws IOException {
+        if (file.isEmpty() || !file.getOriginalFilename().endsWith(".csv")) {
+            throw new IllegalArgumentException("Invalid file. Submit a CSV.");
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+        String firstLine = reader.readLine();
+
+        if (firstLine != null && firstLine.startsWith("\uFEFF")) {
+            firstLine = firstLine.substring(1);
+        }
+
+        List<String> lines = new ArrayList<>();
+        lines.add(firstLine);
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
     }
 }
