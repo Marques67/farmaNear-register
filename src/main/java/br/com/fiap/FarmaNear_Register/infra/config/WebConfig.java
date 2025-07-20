3bacbe9f3ba9af6478c8c110b0000937a972c842
Denner Matheus <dennermat@gmail.com>
2025-07-20 10:39:59 -0300

    creates webconfig with cors policy

---
+++ src/main/java/br/com/fiap/FarmaNear_Register/infra/config/WebConfig.java
@@ -0,0 +1,18 @@
+package br.com.fiap.FarmaNear_Register.infra.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+                .allowedOrigins("")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
